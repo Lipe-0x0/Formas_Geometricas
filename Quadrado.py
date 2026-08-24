72aa2c81da8d82b77494e76bc0e7ee2d78911778
Lipe-0x0 <luisfeli683@gmail.com>
2026-08-24 00:25:34 -0300

    Superfície gerada

--- Quadrado.py
+++ Quadrado.py
@@ -1,1 +1,31 @@
 import numpy as np
+import pygame
+
+
+# Iniciando
+pygame.init()
+
+# Altura e Largura da Tela
+alt = 760
+lar = 760
+
+# Superfície
+screen = pygame.display.set_mode((lar, alt))
+
+# Preenchendo superfície com a cor branca
+screen.fill("white")
+
+# Título da janela
+pygame.display.set_caption("Quadrado")
+
+
+# Mostrando superfície enquanto loop estiver ativo
+while True:
+    
+    # Verificando se "Q" foi pressionado, se sim fechar janela
+    for evento in pygame.event.get():
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_q:
+                pygame.display.quit()
+
+    pygame.display.update()
