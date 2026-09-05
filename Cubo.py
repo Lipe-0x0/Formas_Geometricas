bf3e9300a1e62a5eb18f37ef83d4b9360c6f3f4f
Lipe-0x0 <luisfeli683@gmail.com>
2026-09-05 18:31:16 -0300

    alterei o pacote pygame -> pyglet por conta da centralização da origem

--- Cubo.py
+++ Cubo.py
@@ -1,6 +1,6 @@
 import numpy as np
-import pygame
-
+import pyglet as pg
+from pyglet.math import Mat4
 
 # ------------------ Formas ---------------------
 
@@ -8,7 +8,7 @@ import pygame
 theta = 0.2
 
 # Raio dos pontos
-raio = 10
+raio = 7
 
 # Diametro dos pontos
 diam = raio*2
@@ -16,12 +16,12 @@ diam = raio*2
 # Criação dos 4 pontos no espaço 2D
 pontos = np.array(
     [
-        [350,350,0],
-        [350,380+diam,0],
-        [380+diam,350,0],
-        [380+diam,380+diam,0]
+        [-50,-50,0],
+        [-50,50+diam,0],
+        [50+diam,-50,0],
+        [50+diam,50+diam,0]
      ]
-        )
+       ,dtype = "float64" )
 
 # Criação das arestas
 
@@ -29,93 +29,83 @@ pontos = np.array(
 # Projeção dos pontos no espaço 3D
 Matriz_projec = np.array(
     [
-        [1.10,0,0],
-        [0,1.10,0]
+        [2,0,0],
+        [0,2,0]
     ]
-        )
+       ,dtype = "float64" )
 
 # Matrizes de rotação
 
-rotX = np.array(
+rotZ = np.array(
     [
-        [np.cos(theta), -np.sin(theta), 0],
-        [np.sin(theta), np.cos(theta), 0],
+        [np.cos(theta), np.sin(theta), 0],
+        [-np.sin(theta), np.cos(theta), 0],
         [0,0,1]
     ]
-        )
+        ,dtype = "float64")
     
 
 rotY = np.array(
     [
-        [np.cos(theta), 0, -np.sin(theta)],
+        [np.cos(theta), 0, np.sin(theta)],
         [0,1,0],
-        [np.sin(theta), 0, np.cos(theta)]
+        [-np.sin(theta), 0, np.cos(theta)]
     ]
-        )
+        ,dtype = "float64")
 
 
-rotZ = np.array(
+rotX = np.array(
     [
         [1,0,0],
-        [0,np.cos(theta), -np.sin(theta)],
-        [0,np.sin(theta), np.cos(theta)]
+        [0,np.cos(theta), np.sin(theta)],
+        [0,-np.sin(theta), np.cos(theta)]
     ]
-        )
+        ,dtype = "float64")
 
 
 # ------------------- Canva ---------------------
 
-# Iniciando
-pygame.init()
-
 # Frames por segundo(FPS)
-clock = pygame.time.Clock()
+
 
 # Altura e Largura da Tela
 alt = 760
 lar = 760
 
-# Centro da screen
-centro = np.array([lar/2, alt/2, 0])
-
 # Superfície
-screen = pygame.display.set_mode((lar, alt))
-
-# Título da janela
-pygame.display.set_caption("Quadrado")
+screen = pg.window.Window(lar, alt)
 
 
-# Mostrando superfície enquanto loop estiver ativo
-while True:
+# Redimensionando superfície para que centro seja (0,0)
+@screen.event
+def on_resize(width, height):
+    # Projeção ortográfica 2D
+    screen.projection = Mat4.orthogonal_projection(
+            left = -width//2,
+            right = width//2,
+            bottom = -height//2,
+            top = height//2,
+            z_near = -255,
+            z_far = 255
+            )
 
-    screen.fill("white") # Preenchendo a superfície toda hora com a cor branca
-
-    clock.tick(60) # 30 FPS 
-    
-    # Verificando se tecla foi pressionado para ativar alguma ação
-    for evento in pygame.event.get():
-        if evento.type == pygame.KEYDOWN: # Evento de saída
-            if evento.key == pygame.K_q:
-                pygame.display.quit()
 
 
-    # Desenhando pontos
-    for linha in range(np.shape(pontos)[0]):
-        pygame.draw.circle(screen, "black", (pontos[linha,:][0], pontos[linha,:][1]), raio)
+@screen.event
+def on_draw():
+    screen.clear()
 
-        # Desenhando projeção dos pontos do cubo
-        projec = Matriz_projec @ pontos[linha,:]
-        pygame.draw.circle(screen, "black", (projec[0], projec[1]), raio)
+    for ind, ponto  in enumerate(pontos):
+        # Pontos 2D desenhados
+        pg.shapes.Circle(x = ponto[0], y = ponto[1], radius = raio, color=(255,255,255)).draw()
 
         # Rotacionando em relação ao X-axis da matriz de pontos 2D
-        # Resolvendo problema de rotação em torno da origem
-        # Explicação: pego cada ponto e envio para origem, então rotaciono ele por lá e por fim trago de volta já rotacionado para o centro novamente
-        
-        pontos[linha,:] = rotX @ (pontos[linha,:] - centro) + centro # Rotação x-axis dos vértices
-        
-        # Rotação x-axis das arestas
+        pontos[ind,:] = rotX @ ponto
+
+        # Pontos da projeção desenhados
+        projec = Matriz_projec @ ponto
+        pg.shapes.Circle(x = projec[0], y = projec[1], radius = raio, color=(255,255,255)).draw()
 
 
 
-
-    pygame.display.flip()
+pg.app.run()
