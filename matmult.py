bf4311cb2f5e495a536f1f26451e81fc957d8294
Lipe-0x0 <luisfeli683@gmail.com>
2026-09-16 13:44:40 -0300

    renomeação de arquivo

---
+++ matmult.py
@@ -0,0 +1,14 @@
+
+# Multiplicação de Matrizes
+def matmul(A, B):
+    linhaA = len(A)
+    colunaA = len(A[0])
+
+    linhaB = len(B)
+    colunaB = len(B[0])
+
+    if colunaA == linhaB:
+
+    else:
+        print("colA != rowB")
+        return None
